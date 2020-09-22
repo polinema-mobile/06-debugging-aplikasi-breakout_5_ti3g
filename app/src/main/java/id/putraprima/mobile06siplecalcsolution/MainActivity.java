fc3cd101fa1dcfa6c6d32e3b5eeb085e2c1984d6
rizkyputrapb <rizkyputrapb@gmail.com>
2020-09-22 16:18:46 +0700

    done the task but the validation part.

--- app/src/main/java/id/putraprima/mobile06siplecalcsolution/MainActivity.java
+++ app/src/main/java/id/putraprima/mobile06siplecalcsolution/MainActivity.java
@@ -3,6 +3,7 @@ package id.putraprima.mobile06siplecalcsolution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +13,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button_kali, button_tambah,button_kurang, button_bagi;
     private TextView text_hasil;
     private EditText edit_text_angka_pertama,edit_text_angka_kedua;
-
-    private int angka_pertama, angka_kedua;
+    private static final String TAG = "MainActivity";
+    private int angka_pertama = 0, angka_kedua = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +29,26 @@ public class MainActivity extends AppCompatActivity {
         edit_text_angka_pertama = findViewById(R.id.edit_text_angka_pertama);
 
         //TODO 02 Buatlah kode program untuk menambahkan event klik dari tombol kali dan tombol bagi
+        button_kali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getAngkaMasukan();
+                text_hasil.setText(String.valueOf(angka_pertama*angka_kedua));
+                Log.d(TAG, "perkalian 2 angka");
+            }
+        });
+
+        button_bagi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getAngkaMasukan();
+                text_hasil.setText(String.valueOf(angka_pertama/angka_kedua));
+                Log.d(TAG, "pembagian 2 angka");
+            }
+        });
 
         //TODO 03 Gunakanlah Log Cat Untuk memperbaiki kesalahan program
+
 
         //TODO 04 Gunakanlah Debugger untuk mencari kesalahan program (program berjalan dengan baik namun hasilnya salah)
 
@@ -39,8 +58,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 getAngkaMasukan();
-                text_hasil.setText(angka_pertama+angka_kedua);
-
+                text_hasil.setText(String.valueOf(angka_pertama-angka_kedua));
+                Log.d(TAG, "pengurangan 2 angka");
             }
         });
 
@@ -48,12 +67,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 getAngkaMasukan();
-                text_hasil.setText(angka_pertama+angka_kedua);
+                text_hasil.setText(String.valueOf(angka_pertama+angka_kedua));
+                Log.d(TAG, "penambahan 2 angka");
             }
         });
     }
 
     public void getAngkaMasukan(){
         //TODO 01 Buatlah kode program untuk mengambil nilai input dari edit text
+        this.angka_pertama = Integer.parseInt(edit_text_angka_pertama.getText().toString());
+        this.angka_kedua = Integer.parseInt(edit_text_angka_kedua.getText().toString());
     }
 }
