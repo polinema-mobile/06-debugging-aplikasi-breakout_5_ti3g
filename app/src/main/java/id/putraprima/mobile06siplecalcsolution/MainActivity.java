1c4ccbd612341a2c3f2575ce022e058e23d6fb28
rizkyputrapb <rizkyputrapb@gmail.com>
2020-09-22 16:38:52 +0700

    done the validation part using toast

--- app/src/main/java/id/putraprima/mobile06siplecalcsolution/MainActivity.java
+++ app/src/main/java/id/putraprima/mobile06siplecalcsolution/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
     private Button button_kali, button_tambah,button_kurang, button_bagi;
@@ -28,54 +31,59 @@ public class MainActivity extends AppCompatActivity {
         edit_text_angka_kedua = findViewById(R.id.edit_text_angka_kedua);
         edit_text_angka_pertama = findViewById(R.id.edit_text_angka_pertama);
 
-        //TODO 02 Buatlah kode program untuk menambahkan event klik dari tombol kali dan tombol bagi
-        button_kali.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getAngkaMasukan();
-                text_hasil.setText(String.valueOf(angka_pertama*angka_kedua));
-                Log.d(TAG, "perkalian 2 angka");
-            }
-        });
+            //TODO 02 Buatlah kode program untuk menambahkan event klik dari tombol kali dan tombol bagi
+            button_kali.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    getAngkaMasukan();
+                    text_hasil.setText(String.valueOf(angka_pertama*angka_kedua));
+                    Log.d(TAG, "perkalian 2 angka");
+                }
+            });
 
-        button_bagi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getAngkaMasukan();
-                text_hasil.setText(String.valueOf(angka_pertama/angka_kedua));
-                Log.d(TAG, "pembagian 2 angka");
-            }
-        });
+            button_bagi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    getAngkaMasukan();
+                    text_hasil.setText(String.valueOf(angka_pertama/angka_kedua));
+                    Log.d(TAG, "pembagian 2 angka");
+                }
+            });
 
-        //TODO 03 Gunakanlah Log Cat Untuk memperbaiki kesalahan program
+            //TODO 03 Gunakanlah Log Cat Untuk memperbaiki kesalahan program
 
 
-        //TODO 04 Gunakanlah Debugger untuk mencari kesalahan program (program berjalan dengan baik namun hasilnya salah)
+            //TODO 04 Gunakanlah Debugger untuk mencari kesalahan program (program berjalan dengan baik namun hasilnya salah)
 
-        //TODO 05 Buatlah Validasi input agar masukan yang di isi oleh user tidak membuat program Force Close
+            //TODO 05 Buatlah Validasi input agar masukan yang di isi oleh user tidak membuat program Force Close
 
-        button_kurang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getAngkaMasukan();
-                text_hasil.setText(String.valueOf(angka_pertama-angka_kedua));
-                Log.d(TAG, "pengurangan 2 angka");
-            }
-        });
+            button_kurang.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    getAngkaMasukan();
+                    text_hasil.setText(String.valueOf(angka_pertama-angka_kedua));
+                    Log.d(TAG, "pengurangan 2 angka");
+                }
+            });
 
-        button_tambah.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getAngkaMasukan();
-                text_hasil.setText(String.valueOf(angka_pertama+angka_kedua));
-                Log.d(TAG, "penambahan 2 angka");
-            }
-        });
+            button_tambah.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    getAngkaMasukan();
+                    text_hasil.setText(String.valueOf(angka_pertama+angka_kedua));
+                    Log.d(TAG, "penambahan 2 angka");
+                }
+            });
     }
 
     public void getAngkaMasukan(){
         //TODO 01 Buatlah kode program untuk mengambil nilai input dari edit text
+        try {
         this.angka_pertama = Integer.parseInt(edit_text_angka_pertama.getText().toString());
         this.angka_kedua = Integer.parseInt(edit_text_angka_kedua.getText().toString());
+        }catch (NumberFormatException e){
+            Toast toast1 = Toast.makeText(MainActivity.this, "Invalid input",Toast.LENGTH_SHORT);
+            toast1.show();
+        }
     }
 }
